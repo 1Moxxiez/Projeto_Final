45ec6a7cdb7fce0db75bca6210a17e95c35ef9ca
Moxxie <anaclaramiguel567@gmail.com>
2025-06-17 17:05:31 -0400

    main2

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ import tkinter as tk # Para criar interfaces gráficas
 from tkinter import messagebox # Para exibir mensagens pop-up
 
 # Importa os módulos que contêm as funções lógicas e de dados
-import data_manager       # Gerencia os dados
-import gui_elements       # Funções de interface (como nova janela de texto)
-import event_functions    # Funções para eventos
-import participant_functions # Funções para participantes
-import report_functions   # Funções para relatórios e estatísticas
+# import data_manager       # Gerencia os dados
+# import gui_elements       # Funções de interface (como nova janela de texto)
+# import event_functions    # Funções para eventos
+# import participant_functions # Funções para participantes
+# import report_functions   # Funções para relatórios e estatísticas
 
 
 # -----------------------------------------------------------------
@@ -32,4 +32,45 @@ import report_functions   # Funções para relatórios e estatísticas
 # `Tk()` é a classe que representa a janela raiz de uma aplicação GUI.
 
 root = tk.Tk()  #root (que significa "raiz" em inglês) é o nome padrão dado à janela principal
-root.title('Sstea de Gerenciaento de Eventos da Comunidade Tech')
+root.title('Sistema de Gerenciamento de Eventos da Comunidade Tech') # Define o título da janela
+root.geometry('900x650') # Define o tamanho inicial da janela (largura x altura em pixels)
+
+
+# Informa ao módulo `gui_elements` qual é a janela raiz.
+# Isso é crucial para que `show_in_new_window` possa criar janelas filhas corretamente.
+# gui_elements.set_rot_window(root)
+
+
+
+
+
+# -----------------------------------------------------------------
+# 2. Frame Principal para Organização (Main Frame)
+# -----------------------------------------------------------------
+
+
+'''
+ Um `Frame` é uma prateleira (widget contêiner) que pode agrupar outros objetos (label) widget.
+ Usamos `main_frame` para organizar melhor os elementos dentro da janela `root`.
+
+`pack()` é um gerenciador de layout que organiza widgets em blocos antes de colocá-los
+na janela. 
+
+`expand=True : faz o frame expandir com a janela
+`fill="both" : o faz preencher tanto na horizontal quanto na vertical.
+'''
+main_frame = tk.Frame(root, padx=20, pady=20, bg="lightgray") # Adiciona padding interno ao frame
+main_frame.pack(expand=True, fill='both')
+
+# `root.mainloop()`: Esta linha é a mais importante para iniciar a GUI.
+# Ela inicia o "loop de eventos" do Tkinter. A aplicação fica rodando,
+# esperando por interações do usuário (cliques em botões, digitação, etc.).
+# Sem esta linha, a janela Tkinter apareceria e fecharia imediatamente.
+root.mainloop()
+
+
+
+
+# -----------------------------------------------------------------
+# 3. Títulos e Descrição na Janela Principal
+# -----------------------------------------------------------------
